572b13de0ff41acbc0de75d349606c9250e87f60
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-21 20:25:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def  initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(this_grade)
+
+  end
+end
+
+
+# school = School.new("Bayside High School")
